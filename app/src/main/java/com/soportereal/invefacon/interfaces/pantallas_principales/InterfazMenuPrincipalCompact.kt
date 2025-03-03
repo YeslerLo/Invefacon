7ac9e433027c4926817c1064a2548234407c295f
Yesler <ylorio@soportereal.com>
2025-03-03 04:39:49 -0600

    SE AGREGARON ANIMACIONES EN EL MODULO SAC Y SE MODFIFICO LA INTERFAZ

--- app/src/main/java/com/soportereal/invefacon/interfaces/pantallas_principales/InterfazMenuPrincipalCompact.kt
+++ app/src/main/java/com/soportereal/invefacon/interfaces/pantallas_principales/InterfazMenuPrincipalCompact.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
